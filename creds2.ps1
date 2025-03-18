42127e32658e04babea9e6f3a202725ba0e401ae
Nielsen Pierce <nielsenpi@softcat.com>
2025-03-18 10:37:46 +0000

    Update creds2.ps1

--- creds2.ps1
+++ creds2.ps1
@@ -1,1 +1,1 @@
-Set-AWSCredential -AccessKey "AKIA34BAGDIHVEJI3SFX" -SecretKey "oyAs3Kr3t!" -StoreAs test
+Set-AWSCredential -AccessKey "AKIA34BAGDIHVEJI3SFX" -SecretKey "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" -StoreAs test
