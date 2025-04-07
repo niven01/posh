93654e50c42aba25c58ed2607d8f0c488532f2d6
Nielsen Pierce <nielsenpi@softcat.com>
2025-04-07 14:56:32 +0100

    Update creds4.ps1

--- creds4.ps1
+++ creds4.ps1
@@ -1,2 +1,2 @@
-Set-AWSCredential -AccessKey "AKIA56BAGDIHVEJI4SVB" -SecretKey "ogfsdfsdfsdfsd" -StoreAs test
+Set-AWSCredential -AccessKey "AKIAIOSFODNN7EXAMPLE" -SecretKey "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" -StoreAs test
 Password = "AP4ssw0rd"
